86867f3ab97e3699216d1c19bf9f44aadf5cc712
Gabriel <136838289+GabD-S@users.noreply.github.com>
2024-12-26 11:57:02 -0300

    Delete backend directory

--- backend/src/prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient({
-  errorFormat: "minimal",
-  log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : ["warn"],
-});
